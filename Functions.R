e9f7442f0780376e935cd52a7ef68f55a9405e9e
Matt <mattnolan001@gmail.com>
2020-06-06 14:01:41 +0100

    Add functions from original project.

--- Functions.R
+++ Functions.R
@@ -1,2 +1,3 @@
 # Functions
-# Moved from functions.Rmd in original project repository.
+# Moved from functions.Rmd in original project repository at https://github.com/MattNolanLab/Inter_Intra_Variation.
+
